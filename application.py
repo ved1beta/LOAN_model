c6488752f535cf87773257b1a2e46a079eb31f68
Vedant <146507396+ved1beta@users.noreply.github.com>
2024-12-16 10:56:11 +0530

    Update application.py

--- application.py
+++ application.py
@@ -41,4 +41,4 @@ def predict_datapoint():
             return render_template('home.html', error="An error occurred during prediction.")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host= '0.0.0.0', port = 8000)
